c8407056e88b46a7018beda7626ae1b794832a95
Choi-jeonghoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2022-05-25 15:18:45 +0900

    Add: first commit. 초기 세팅 완료.

---
+++ src/Router.js
@@ -0,0 +1,40 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginJh from "./pages/jeonghoon/Login/Login";
+import MainJh from "./pages/jeonghoon/Main/Main";
+
+import LoginJy from "./pages/jiYook/Login/Login";
+import MainJy from "./pages/jiYook/Main/Main";
+
+import LoginKim from "./pages/kimminuk/Login/Login";
+import MainKim from "./pages/kimminuk/Main/Main";
+
+import LoginSm from "./pages/sangmin/Login/Login";
+import MainSm from "./pages/sangmin/Main/Main";
+
+import LoginSj from "./pages/sujin/Login/Login";
+import MainSj from "./pages/sujin/Main/Main";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login-Jh" element={<LoginJh />} />
+        <Route path="/main-Jh" element={<MainJh />} />
+
+        <Route path="/login-Jy" element={<LoginJy />} />
+        <Route path="/main-Jy" element={<MainJy />} />
+
+        <Route path="/login-Kim" element={<LoginKim />} />
+        <Route path="/main-Kim" element={<MainKim />} />
+
+        <Route path="/login-Sm" element={<LoginSm />} />
+        <Route path="/main-Sm" element={<MainSm />} />
+
+        <Route path="/login-Sj" element={<LoginSj />} />
+        <Route path="/main-Sj" element={<MainSj />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
