9dbdc9be24bb82ad595e391265024f590c772100
Sujin Choi <hissujinc@gmail.com>
2022-05-31 16:21:43 +0900

    Fix: delete comments.js

--- src/pages/sujin/Main/Feed/Feed.js
+++ src/pages/sujin/Main/Feed/Feed.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import Comment from "../Comment/Comment";
 import './Feed.scss';
